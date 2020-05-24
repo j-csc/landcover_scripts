6f968fec07c05a8515dee19bed039f3aaff4efdb
jaloo555 <superj63@hotmail.com>
2020-05-23 19:07:19 -0700

    sample patches

---
+++ generate_training_patches.py
@@ -0,0 +1,81 @@
+import glob
+import rasterio
+import fiona
+import numpy as np
+import keras.utils
+
+def gen_training_patches(x_fns, y_fns, width, height, channel, batch_size):
+    x_batches = np.zeros((batch_size, width, height, channel), dtype=np.float32)
+    y_batches = np.zeros((batch_size, width, height), dtype=np.float32)
+
+    print(x_batches.shape, y_batches.shape)
+
+    ground_truth_set = glob.glob(y_fns + "*")
+
+    count = 0
+
+    non_zero_count = 0
+
+    while count < batch_size:
+        # Randomly choose a file from input list
+        y_fn = np.random.choice(ground_truth_set)
+        folder_name = y_fn.split('/')[2][2:7]
+        filename = y_fn.split('/')[2][:26]
+        x_fn = x_fns + folder_name + "/" + filename + ".mrf"
+
+        # Load input file
+        f = rasterio.open(x_fn, "r")
+        data = f.read().squeeze()
+        data = np.rollaxis(data, 0, 3)
+        f.close()
+        # print(data.shape)
+
+        # Load ground truth file
+        f = rasterio.open(y_fn, "r")
+        target = f.read().squeeze()
+        f.close()
+        print(target.shape)
+
+        # Randomly sample 100 240x240 patch per file
+        for i in range(100):
+            if count != batch_size:
+                x = np.random.randint(0, data.shape[1]-width)
+                y = np.random.randint(0, data.shape[0]-height)
+                while np.any((data[y:y+height, x:x+width, :] == 0).sum(axis=2) == data.shape[2]):
+                    x = np.random.randint(0, data.shape[1]-width)
+                    y = np.random.randint(0, data.shape[0]-height)
+                    
+                img = data[y:y+height, x:x+width, :].astype(np.float32)
+                temp_target = target[y:y+height, x:x+width].copy()
+
+                if (np.count_nonzero(temp_target) != 0):
+                    non_zero_count += 1
+
+                x_batches[count] = img
+                y_batches[count] = temp_target
+                # print(np.count_nonzero(y_batches[count]))
+                count += 1
+
+                if count % 1000 == 0:
+                    print(f"Iteration: {count}")
+        del data, target
+
+    x_batches = x_batches/255.0
+    y_batches = keras.utils.to_categorical(y_batches, num_classes=2)
+
+    # np.save("./x_train.npy",x_batches)
+    # np.save("./y_train.npy",y_batches)
+
+    print(non_zero_count / count)
+
+    return x_batches, y_batches
+
+def main():
+    # Sample 50k patches of 240x240 images
+    gen_training_patches("../../../media/disk2/datasets/maaryland_naip_2017/",
+     "./binary_raster_md_tif/", 240, 240, 4, 50000)
+    
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ generate_tuned_model_v3.py
@@ -0,0 +1,169 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# pylint: disable=E1137,E1136,E0110
+
+import sys, os, time
+import joblib
+import argparse
+import numpy as np
+import glob
+import json
+import rasterio
+import fiona
+import fiona.transform
+
+import keras
+import keras.backend as K
+import keras.callbacks
+import keras.utils
+from keras.optimizers import SGD, Adam, RMSprop, Adadelta
+from keras.layers import Input, Dense, Activation, MaxPooling2D, Conv2D, BatchNormalization
+from keras.layers import Concatenate, Cropping2D, Lambda
+from keras.losses import categorical_crossentropy
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint
+import pandas as pd
+from scipy.signal import convolve2d
+import custom_loss
+import generate_training_patches
+
+# Sample: python generate_tuned_model_v3.py --in_geo_path ./binary_raster_md/ --in_tile_path ./binary_raster_md_tif/ --in_model_path_sup ../landcover-old/web_tool/data/naip_demo_model.h5 --in_model_path_ae ../landcover-old/web_tool/data/naip_autoencoder.h5 --out_model_path_sup ./naip_demo_tuned.h5 --out_model_path_ae ./naip_autoencoder_tuned.h5 --num_classes 2 --gpu 1 --exp 1 --even even
+
+def masked_categorical_crossentropy(y_true, y_pred):
+    
+    mask = K.all(K.equal(y_true, [1,0,0,0,0,0]), axis=-1)
+    mask = 1 - K.cast(mask, K.floatx())
+
+    loss = K.categorical_crossentropy(y_true, y_pred) * mask
+
+    return K.sum(loss) / K.sum(mask)
+
+keras.losses.masked_categorical_crossentropy = masked_categorical_crossentropy
+
+def get_loss(mask_value):
+    mask_value = K.variable(mask_value)
+    def masked_categorical_crossentropy(y_true, y_pred):
+        
+        mask = K.all(K.equal(y_true, mask_value), axis=-1)
+        mask = 1 - K.cast(mask, K.floatx())
+
+        loss = K.categorical_crossentropy(y_true, y_pred) * mask
+
+        return K.sum(loss) / K.sum(mask)
+    return masked_categorical_crossentropy
+
+def get_model(model_path, num_classes):
+    # K.clear_session()
+    tmodel = keras.models.load_model(model_path, custom_objects={
+        "jaccard_loss":keras.metrics.mean_squared_error,
+        "loss":keras.metrics.mean_squared_error,
+        "masked_categorical_crossentropy":keras.metrics.mean_squared_error
+    })
+    toutput = tmodel.layers[-2].output
+    toutput = Conv2D(num_classes+1, (1,1), padding="same", use_bias=True, activation="softmax", name="output_conv")(toutput)
+    model = keras.models.Model(inputs=tmodel.inputs, outputs=[toutput])
+
+    optimizer = Adam(lr=0.001)
+    loss_mask = np.zeros(num_classes+1)
+    loss_mask[0] = 1
+
+    model.compile(loss=get_loss(loss_mask), optimizer=optimizer)
+    
+    return model
+
+def train_model_from_points(in_geo_path, in_model_path_sup, in_model_path_ae, in_tile_path, out_model_path_sup, out_model_path_ae, num_classes, exp, even):
+    # Train supervised
+    print("Loading initial models...")
+    model_sup = get_model(in_model_path_sup, num_classes)
+    model_sup.summary()
+
+    # Load in sample
+    print("Loading tiles...")
+    x_train, y_train = generate_training_patches.gen_training_patches("../../../media/disk2/datasets/maaryland_naip_2017/",
+     "./binary_raster_md_tif/", 240, 240, 4, 50000)
+
+    print(x_train.shape)
+    print(y_train.shape)
+
+    # y_train_ae[:,:,:] = [1] + [0] * (y_train_ae.shape[-1]-1)
+    # y_train[:,:,:] = [1] + [0] * (y_train.shape[-1]-1)
+
+    # x_train = x_train / 255.0
+    # x_train_ae = x_train_ae / 255.0
+
+    # Supervised tuning
+
+    print("Tuning supervised model")
+
+    cpPath = f"./{exp}/tmp_sup_{even}/sup_tuned_model_{even}_"
+
+    checkpointer_sup = ModelCheckpoint(filepath=(cpPath+"{epoch:02d}_{loss:.2f}.h5"), monitor='loss', verbose=1)
+
+    model_sup.fit(
+        x_train, y_train,
+        batch_size=10, epochs=10, verbose=1, validation_split=0,
+        callbacks=[checkpointer_sup]
+    )
+
+    model_sup.save(out_model_path_sup)
+
+    # # Unsupervised tuning
+
+    # print("Tuning Unsupervised model")
+
+    # model_ae = get_model(in_model_path_ae, num_classes)
+    # model_ae.summary()
+
+    # cpPath = f"{exp}/tmp_ae_{even}/ae_tuned_model_{even}_"
+
+    # checkpointer_ae = ModelCheckpoint(filepath=(cpPath+"{epoch:02d}_{loss:.2f}.h5"), monitor='loss', verbose=1)
+
+    # model_ae.fit(
+    #     x_train_ae, y_train_ae,
+    #     batch_size=10, epochs=10, verbose=1, validation_split=0,
+    #     callbacks=[checkpointer_ae]
+    # )
+
+    # model_ae.save(out_model_path_ae)
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate a model tuned using webtool")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose debugging", default=False)
+    # Input
+    parser.add_argument("--in_geo_path", action="store", dest="in_geo_path", type=str, help="Input geojson dir path (i.e. ../data/output.geojson)", required=True)
+    parser.add_argument("--in_tile_path", action="store", dest="in_tile_path", type=str, help="Path to input tif dir", required=True)
+    # Models
+    parser.add_argument("--in_model_path_sup", action="store", dest="in_model_path_sup", type=str, help="Path to supervised model that needs retraining", required=True)
+    parser.add_argument("--in_model_path_ae", action="store", dest="in_model_path_ae", type=str, help="Path to unsupervised model that needs retraining", required=True)
+    parser.add_argument("--out_model_path_sup", action="store", dest="out_model_path_sup", type=str, help="Output path for tuned supervised model", required=True)
+    parser.add_argument("--out_model_path_ae", action="store", dest="out_model_path_ae", type=str, help="Output path for tuned unsupervised model", required=True)
+    # Custom
+    parser.add_argument("--num_classes", action="store", dest="num_classes", type=str, help="Number of classes", required=True)
+    parser.add_argument("--gpu", action="store", dest="gpuid", type=int, help="GPU to use", required=True)
+    # Experiment argument
+    parser.add_argument("--exp", action="store",dest="exp", type=str, required=True)
+    parser.add_argument("--even", action="store",dest="evenodd", type=str, required=True)
+
+    args = parser.parse_args(sys.argv[1:])
+    args.batch_size=10
+    args.num_epochs=30
+
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpuid)
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = "3"
+
+    start_time = float(time.time())
+
+    print("GeoJson file read at {}".format(args.in_geo_path))
+
+    print("Retraining model from GeoJson")
+
+    train_model_from_points(args.in_geo_path, args.in_model_path_sup, args.in_model_path_ae, args.in_tile_path, args.out_model_path_sup, args.out_model_path_ae, int(args.num_classes), args.exp, args.evenodd)
+
+    print("Finished in %0.4f seconds" % (time.time() - start_time))
+    
+    pass
+
+if __name__ == "__main__":
+    main()
